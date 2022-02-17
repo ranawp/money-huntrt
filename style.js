4d467497ea7a795a1dc92c6a43600fa9fa7848eb
ranawp <ranasaha4224#gmail.com>
2022-02-17 23:45:25 +0600

    error massage handeling

--- style.js
+++ style.js
@@ -1,15 +1,18 @@
 //function call of total balance 
 function claculateButton() {
-    let foodInput = document.getElementById('food-input')
-    let rentInput = document.getElementById('rent-input')
-    let clothesInput = document.getElementById('clothes-input')
-    let totalExpences = document.getElementById('total-expences')
-    let foodValue = foodInput.value
-    let rentIValue = rentInput.value
-    let clothesValue = clothesInput.value
+    let foodInput = document.getElementById('food-input');
+    let rentInput = document.getElementById('rent-input');
+    let clothesInput = document.getElementById('clothes-input');
+    let totalExpences = document.getElementById('total-expences');
+    let foodValue = foodInput.value;
+    let rentIValue = rentInput.value;
+    let clothesValue = clothesInput.value;
 
-    let incomeInput = document.getElementById('income-Input')
-    const errorInput = document.getElementById('error-input')
+
+
+
+    let incomeInput = document.getElementById('income-Input');
+    const errorInput = document.getElementById('error-input');
     if (incomeInput.value > 0 && foodValue > 0 && rentIValue > 0 && clothesValue > 0) {
 
         let total = parseInt(foodValue) + parseInt(rentIValue) + parseInt(clothesValue)
@@ -17,27 +20,28 @@ function claculateButton() {
 
         //calculate total balance 
 
-        let balanceText = document.getElementById('total-balance')
-        let incomeNumber = incomeInput.value
-        let expencesNumber = totalExpences.innerText
+        let balanceText = document.getElementById('total-balance');
+        let incomeNumber = incomeInput.value;
+        let expencesNumber = totalExpences.innerText;
 
 
-        let incomeTotal = parseInt(incomeNumber) - parseInt(expencesNumber)
+        let incomeTotal = parseInt(incomeNumber) - parseInt(expencesNumber);
 
-        balanceText.innerText = incomeTotal
+        balanceText.innerText = incomeTotal;
 
 
     } else {
 
-        errorInput.classList.remove('d-none')
+        errorInput.classList.remove('d-none');
+
     }
     // error massage of expences
-    const errorExpences = document.getElementById('error-expense')
-    if (totalExpences.innerText > incomeInput.value) {
+    const errorExpences = document.getElementById('error-expense');
+
+    if (incomeInput.value < totalExpences.innerText) {
         errorExpences.classList.remove('d-none')
-    } else if (totalExpences.innerText < incomeInput.value) {
-        errorExpences.classList.add('d-none')
     }
+
 }
 // calculate total input values
 document.getElementById('Calculate-button').addEventListener('click', function () {
@@ -58,10 +62,7 @@ function calculateSavings() {
     //error massage of savings
     const savingsError = document.getElementById('savings-error')
     if (saveAmount.innerText > balanceText.innerText) {
-        savingsError.classList.remove('d-none');
-    } else {
-        savingsError.classList.add('d-none');
-        console.log('ok')
+        alert('Your savings should lower than your Balance')
     }
     //calculate remaining balance  
     let balance = document.getElementById('total-balance')
